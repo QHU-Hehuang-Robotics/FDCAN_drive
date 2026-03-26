aad068b0d2fe5eb48fdb2ea7b614316c67c724d5
tzh <2735604537@qq.com>
2026-03-26 18:51:25 +0800

    ADD FDCAN_Filter_Config

--- DM_FDCAN/CAN_drive.c
+++ DM_FDCAN/CAN_drive.c
@@ -1,16 +1,36 @@
 #include "CAN_drive.h"
 
-
-void CAN_TxHeader_Init(FDCAN_TX_DATA_t data,CAN_TxHeaderTypeDef DM_TxHeaderTypeDef)
+void FDCAN_Filter_Config(FDCAN_MsgPacket_t *packet, const FDCAN_FilterConf_t *conf)
 {
-    DM_TxHeaderTypeDef.Identifier           = data.can_id;
-    DM_TxHeaderTypeDef.IdType               = CAN_STANDARD;
-    DM_TxHeaderTypeDef.TxFrameType          = CAN_DATA_FRAME;
-    DM_TxHeaderTypeDef.DataLength           = CAN_Length;
-    DM_TxHeaderTypeDef.ErrorStateIndicator  = CAN_ERROR_MOD;
-    DM_TxHeaderTypeDef.BitRateSwitch        = CAN_BRS_MOD;
-    DM_TxHeaderTypeDef.FDFormat             = CAN_MOD;
-    DM_TxHeaderTypeDef.TxEventFifoControl   = CAN_EFC;
-    DM_TxHeaderTypeDef.MessageMarker        = CAN_MESS;
+    FDCAN_FilterTypeDef tmpFilter;
+
+    tmpFilter.IdType       = CAN_STANDARD;
+    tmpFilter.FilterIndex  = conf->Index;
+    tmpFilter.FilterType   = conf->Type;
+    tmpFilter.FilterConfig = conf->Config;
+    tmpFilter.FilterID1    = conf->Id1;
+    tmpFilter.FilterID2    = conf->Id2;
+    tmpFilter.RxBufferIndex = 0;
+    tmpFilter.IsCalibrationMsg = 0;
+
+    HAL_FDCAN_ConfigFilter(packet->hfdcan, &tmpFilter);
 }
 
+void FDCAN_FilterHeader_Init(FDCAN_MsgPacket_t *packet, uint8_t index, uint32_t type, uint32_t config, uint32_t id1, uint32_t id2)
+{
+    if (packet->hfdcan == NULL) return;
+
+    packet->FilterHeader.IdType       = CAN_STANDARD;
+    packet->FilterHeader.FilterIndex  = index;
+    packet->FilterHeader.FilterType   = type;
+    packet->FilterHeader.FilterConfig = config;
+    packet->FilterHeader.FilterID1    = id1;
+    packet->FilterHeader.FilterID2    = id2;
+    packet->FilterHeader.RxBufferIndex = 0;
+    packet->FilterHeader.IsCalibrationMsg = 0;
+
+    if (HAL_FDCAN_ConfigFilter(packet->hfdcan, &packet->FilterHeader) != HAL_OK) {
+        packet->State = CAN_STATE_CONFIG_ERROR;
+        return;
+    }
+}
--- DM_FDCAN/CAN_drive.h
+++ DM_FDCAN/CAN_drive.h
@@ -1,24 +1,81 @@
-#ifndef __CAN_drive_h
-#define __CAN_drive_h
+#ifndef __CAN_DRIVE_H
+#define __CAN_DRIVE_H
 
-#include "cmsis_os.h"
-#include "cordic.h"
-#include "fdcan.h"
-#include "gpio.h"
+#include "main.h"
+
+#define FDCAN_MOD 
+
+#ifdef FDCAN_MOD
 
 typedef struct {
     uint32_t can_id;
     uint8_t* data;
     uint16_t len;
-}FDCAN_TX_DATA_t;
+} FDCAN_TX_DATA_t;
 
-#define CAN_TxHeaderTypeDef     FDCAN_TxHeaderTypeDef
+typedef struct {
+    uint8_t  Index;      // 过滤器索引 (0-127)
+    uint32_t Type;       // 模式 (MASK / RANGE / DUAL)
+    uint32_t Config;     // 去向 (FIFO0 / FIFO1 / REJECT)
+    uint32_t Id1;        // 参数1 (目标ID 或 起始ID)
+    uint32_t Id2;        // 参数2 (掩码 或 结束ID)
+} FDCAN_FilterConf_t;
+
+typedef enum {
+    CAN_STATE_IDLE = 0,      // 空闲
+    CAN_STATE_READY,         // 就绪
+    CAN_STATE_STARTING,      // 启动中
+    CAN_STATE_NORMAL,        // 正常运行
+    CAN_STATE_BUSY_TX,       // 发送忙
+    CAN_STATE_BUSY_RX,       // 接收忙
+    CAN_STATE_SLEEP,         // 睡眠
+    CAN_STATE_ERROR_WARNING, // 错误警告 (>96)
+    CAN_STATE_ERROR_PASSIVE, // 被动错误 (>127)
+    CAN_STATE_BUS_OFF,       // 总线离线
+    CAN_STATE_CONFIG_ERROR,  // 配置错误
+    CAN_STATE_TIMEOUT        // 超时
+} CAN_State_t;
+
+typedef struct {
+    FDCAN_HandleTypeDef* hfdcan;
+    CAN_State_t              State;
+    FDCAN_TxHeaderTypeDef    TxHeader;
+    FDCAN_RxHeaderTypeDef    RxHeader;
+    FDCAN_FilterTypeDef      FilterHeader;
+    __attribute__((aligned(4))) uint8_t Data[64];
+} FDCAN_MsgPacket_t;
+
 #define CAN_STANDARD            FDCAN_STANDARD_ID
+#define CAN_EXTENDED            FDCAN_EXTENDED_ID
 #define CAN_DATA_FRAME          FDCAN_DATA_FRAME
 #define CAN_MOD                 FDCAN_CLASSIC_CAN
 #define CAN_Length              FDCAN_DLC_BYTES_8
+
 #define CAN_ERROR_MOD           FDCAN_ESI_ACTIVE
 #define CAN_BRS_MOD             FDCAN_BRS_OFF
 #define CAN_EFC                 FDCAN_NO_TX_EVENTS
 #define CAN_MESS                0
-#endif
+
+
+#define CAN_FILTER_MASK         FDCAN_FILTER_MASK
+#define CAN_FILTER_RANGE        FDCAN_FILTER_RANGE
+#define CAN_FILTER_DUAL         FDCAN_FILTER_DUAL
+#define CAN_FIFO0               FDCAN_FILTER_TO_RXFIFO0
+#define CAN_FIFO1               FDCAN_FILTER_TO_RXFIFO1
+#define CAN_REJECT_UNMATCHED    FDCAN_REJECT
+#define CAN_REJECT_REMOTE       FDCAN_FILTER_REMOTE
+#define CAN_MASK_ALL            0x000
+#define CAN_MASK_EXACT          0x7FF
+
+#define CAN_START(h)    do { \
+    HAL_FDCAN_Start(h); \
+    HAL_FDCAN_ActivateNotification(h, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0); \
+    HAL_FDCAN_ActivateNotification(h, FDCAN_IT_RX_FIFO1_NEW_MESSAGE, 0); \
+} while(0)
+
+/* --- 6. 函数声明 --- */
+void FDCAN_TxHeader_Init(FDCAN_MsgPacket_t *packet, uint32_t can_id);
+void FDCAN_FilterHeader_Init(FDCAN_MsgPacket_t *packet, uint8_t index, uint32_t type, uint32_t config, uint32_t id1, uint32_t id2);
+
+#endif /* FDCAN_MOD */
+#endif /* __CAN_DRIVE_H */
