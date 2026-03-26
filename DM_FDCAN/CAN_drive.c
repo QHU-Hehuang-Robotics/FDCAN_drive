0f461524e79cdd682274f753b6269e6325dc437f
tzh <2735604537@qq.com>
2026-03-26 18:53:08 +0800

    ADD FDCAN_TxHeader_Init

--- DM_FDCAN/CAN_drive.c
+++ DM_FDCAN/CAN_drive.c
@@ -34,3 +34,19 @@ void FDCAN_FilterHeader_Init(FDCAN_MsgPacket_t *packet, uint8_t index, uint32_t 
         return;
     }
 }
+
+void FDCAN_TxHeader_Init(FDCAN_MsgPacket_t *packet, uint32_t can_id)
+{
+    packet->TxHeader.Identifier          = can_id;
+    packet->TxHeader.IdType              = CAN_STANDARD;
+    packet->TxHeader.TxFrameType         = CAN_DATA_FRAME;
+    packet->TxHeader.DataLength          = CAN_Length;
+    packet->TxHeader.ErrorStateIndicator = CAN_ERROR_MOD;
+    packet->TxHeader.BitRateSwitch       = CAN_BRS_MOD;
+    packet->TxHeader.FDFormat            = CAN_MOD;
+    packet->TxHeader.TxEventFifoControl  = CAN_EFC;
+    packet->TxHeader.MessageMarker       = CAN_MESS;
+    
+    packet->State = CAN_STATE_READY;
+}
+
